3162b34079daf7bac6b64881f6ee5e4145d2ee3e
Noor Huseen <noorhuseen999@gmail.com>
2026-04-30 21:06:20 +0300

    Testing full CI/CD pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "DevOps Project is Running!"
+    return "DevOps Pipeline is LIVE and Fully Automated!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
