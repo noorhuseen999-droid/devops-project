2377cc362e28e5439088a482c2212c6a32748f04
Noor Huseen <noorhuseen999@gmail.com>
2026-05-04 20:35:56 +0300

    Trigger pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Auto Deploy is Working!"
+    return "Final DevOps Pipeline is Live!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
